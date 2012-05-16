f8ea7ca4eda6933352b684b2bb2954b2509ace3b
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2012-05-16 22:52:22 +0700

    maven power

---
+++ src/main/java/proto/Launcher.java
@@ -0,0 +1,8 @@
+package proto;
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/test/java/proto/LauncherTest.java
@@ -0,0 +1,17 @@
+package proto;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+
+public class LauncherTest extends TestCase {
+	
+	public static Test suite() {
+		return new TestSuite(LauncherTest.class);
+	}
+	
+
+	public static void testMain() {
+		assertTrue(true);
+	}
+}
