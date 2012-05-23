b58f56870c35ec24c71d256102135da2ede7af87
Vladimir Kostyukov <vladimir.kostukov@gmail.com>
2012-05-23 23:36:28 +0700

    cli implemented

--- src/main/java/proto/Launcher.java
+++ src/main/java/proto/Launcher.java
@@ -1,28 +1,57 @@
 package proto;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.antlr.runtime.ANTLRFileStream;
 import org.antlr.runtime.RecognitionException;
-import org.stringtemplate.v4.compiler.CodeGenerator.includeExpr_return;
 
 import proto.core.Compiler;
 import proto.core.TinyCompiler;
 
 public class Launcher {
 
-	public static void main(String[] args) throws IOException, RecognitionException {
-		// парсить командную строку
-		// + создавать инстанс компилятора (+парсер, лексер, генератор)
-		// + запускать compiler.compile
-		// generator.generate
+	public void launch(Map<String, String> args) throws IOException, RecognitionException {
 
 		ANTLRFileStream fs = new ANTLRFileStream("examples/simple.proto");
-
+		
 		Compiler compiler = new TinyCompiler();
 		String output = "";
 		output = compiler.compile(fs);
 		
 		System.err.println(output);
+	}
+
+	public static void usage() {
+		String usage = "Welcome to Proto Compiler 0.0.1! [http://github.com/hisohito/proto-code]\n\n" +
+		"USAGE:\n" +
+		"\t java -cp proto-0.0.1.jar proto.Launcher [--target=<TARGET>] [--output=<OUTPUT_FILE>] <SOURCE>\n\n" +
+		"\t <SOURCE> is an proto source file\n" +
+		"\t <TARGET> is one of supported targets (promela by default)\n" +
+		"\t <OUTPUT_FILE> is an output file name (out.pml by default)\n";
+		
+		System.out.println(usage);
+	}
+
+	public static void main(String[] args) throws IOException, RecognitionException {
+		
+		Map<String, String> yeld = new HashMap<String, String>();
+		for(String arg: args) {
+			if (arg.matches("--\\w+=\\w+")) {
+				arg = arg.substring(2);
+				String[] parts = arg.split("=");
+				yeld.put(parts[0], parts[1]);
+			} else {
+				yeld.put("source", arg);
+			}
+		}
+		
+		if (yeld.get("source") == null) {
+			usage();
+			return;
+		}
+		
+		new Launcher().launch(yeld);
 	}
 }
